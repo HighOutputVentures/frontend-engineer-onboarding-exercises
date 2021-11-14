7be58e3eb798bcb7afe92f720e832d32bc796444
vmc08 <vmc08@yahoo.com>
2021-11-15 01:26:13 +0800

    refactor(app): add support for react-18 concurrent mode and server components

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@ module.exports = {
     appName: 'HOV.fe',
     appDescription: 'Build to boost development start up vs starting from scratch.',
   },
+  /**
+   * https://nextjs.org/docs/advanced-features/react-18
+   */
+  concurrentFeatures: true,
+  serverComponents: true,
 }
